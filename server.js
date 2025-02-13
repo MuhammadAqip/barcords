8583f1712aeaa759ee6bc0cf7802150dd4da84ce
Muhammad Aqip <aqipsani@gmail.com>
2025-02-14 00:38:30 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www')
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+
+var __path = process.cwd()
+
+const app = express()
+app.use(cors())
+app.use(secure)
+
+app.get('/', function(req, res) {
+    console.log(req.rawHeaders[0])
+    res.sendFile(__path + '/LIBA RECORD - Home.html')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is Running !!! at Port ' + PORT)
+})
